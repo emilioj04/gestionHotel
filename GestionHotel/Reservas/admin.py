1b807c20c16c18d7803322f6871131200ac29c8a
josutrres <josutrres@gmail.com>
2024-06-03 23:03:37 -0400

    models sketch 1.0.1

--- GestionHotel/Reservas/admin.py
+++ GestionHotel/Reservas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Habitacion, Servicio
+from .models import Habitacion, Servicio, Reserva
 # Register your models here.
 admin.site.register(Habitacion)
 admin.site.register(Servicio)
+admin.site.register(Reserva)
--- GestionHotel/Reservas/models.py
+++ GestionHotel/Reservas/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from Empleados.models import Empleado
+from Huespedes.models import Huesped
+
 
 class TipoHabitacion(models.TextChoices):
     SENCILLA = 'S', 'Sencilla'
@@ -9,6 +11,7 @@ class TipoHabitacion(models.TextChoices):
     PRESIDENCIAL = 'P', 'Presidencial'
     VIP = 'V', 'VIP'
     FAMILIAR = 'F', 'Familiar'
+
 
 class Habitacion(models.Model):
     numero = models.IntegerField(editable=False)
@@ -33,8 +36,26 @@ class TipoServicio(models.TextChoices):
 class Servicio(models.Model):
     tipoServicio = models.CharField(max_length=15, choices=TipoServicio.choices, default=TipoServicio.SPA)
     costo = models.DecimalField(max_digits=10, decimal_places=2)
-    empleado_list = models.ManyToManyField(Empleado, related_name='servicio_list')
+    empleado_list = models.ForeignKey(Empleado, related_name='servicios', on_delete=models.DO_NOTHING,default=None)
 
     def __str__(self):
         return self.tipoServicio
 
+
+class EstadoReserva(models.TextChoices):
+    RESERVADA = 'RESERVADA', 'RESERVADA'
+    CANCELADA = 'CANCELADA', 'CANCELADA'
+    LIBERADA = 'LIBERADA', 'LIBERADA'
+
+
+class Reserva(models.Model):
+    fechaEntrada = models.DateField()
+    fechaSalida = models.DateField()
+    estado = models.CharField(max_length=10, choices=EstadoReserva.choices, default=EstadoReserva.RESERVADA)
+    nroHabitaciones = models.IntegerField()
+    huesped = models.ForeignKey(Huesped, on_delete=models.DO_NOTHING, related_name='reservas')
+    habitacionList = models.ForeignKey(Habitacion, on_delete=models.DO_NOTHING, related_name='reservas',default=None)
+    servicioList = models.ForeignKey(Servicio, related_name='reservas',default=None, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(self.idReserva)
